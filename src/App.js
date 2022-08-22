94e3e39d518c97c0d609432c396b664c5a0b1ac4
Konstantin Shambazov <k.shambazov@gmail.com>
2022-08-22 15:59:35 +0900

    feat: add login form functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import Container from "@mui/material/Container";
 import { Header } from "./components/Header"
 import { Home } from "./pages/Home"
-
+import { Login } from "./pages/Login"
 function App() {
   return (
     <>
       <Header />
       <Container maxWidth="lg">
         <Home />
+        <Login />
       </Container>
     </>
   );
